58edc4317ad2a5d5602f210f7dc970107db56a6b
Muhammad Zubair <muhammadzubairmalik118@gmail.com>
2026-05-17 10:47:23 +0500

    Pandas Series Functions

--- panadas_learning.py
+++ panadas_learning.py
@@ -8,7 +8,14 @@ import pandas as pd
 s = pd.Series([10,20,20,30,40], index = ['a','b','c','d','e'])
 print(s)
 
-#This function is use to print the values of the series
+#This function is use to print the only values of the series
 print("Values: ", s.values) #1st Method to print the value of the series
 values = s.values #1st Method to print the value of the series
 print("Values are: ", values)
+
+#This function is use to print the only index of the series
+print("Indexs: ", s.index) #1st Method to print the indexes of the series
+
+print("Type: ", s.dtype) #Print the type of the series
+print("Header: ", s.head) #Print the head of the series
+print("Tail: ", s.tail) #Print the tail of the series
