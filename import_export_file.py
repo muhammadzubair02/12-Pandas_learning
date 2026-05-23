40c763bf7ecbdc62cefff12d8e2b33b68087a993
Muhammad Zubair <muhammadzubairmalik118@gmail.com>
2026-05-23 09:29:12 +0500

    read csv file

--- import_export_file.py
+++ import_export_file.py
@@ -1,2 +1,10 @@
 import pandas as pd
 import openpyxl #we install the library openpyxl for handle the excel file
+
+df = pd.read_csv("HousePricePrediction.csv",
+                 parse_dates=['Id'],
+                 dtype={'LotArea': 'float64',
+                        'SalePrice': 'float64' })
+
+# print(df.columns)
+print(df.info())
