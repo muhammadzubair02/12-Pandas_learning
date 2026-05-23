bf01cb0bafcfa698de698bdfe36fd3ddc7cf089a
Muhammad Zubair <muhammadzubairmalik118@gmail.com>
2026-05-23 10:30:45 +0500

    export the file name subset house price

--- import_export_file.py
+++ import_export_file.py
@@ -1,10 +1,21 @@
 import pandas as pd
 import openpyxl #we install the library openpyxl for handle the excel file
 
-df = pd.read_csv("HousePricePrediction.csv",
-                 parse_dates=['Id'],
-                 dtype={'LotArea': 'float64',
-                        'SalePrice': 'float64' })
+# df = pd.read_csv("HousePricePrediction.csv",
+#                  parse_dates=['Id'],
+#                  dtype={'LotArea': 'float64',
+#                         'SalePrice': 'float64' })
 
 # print(df.columns)
-print(df.info())
+#print(df.info())  #Show the info of data frame with a bove values
+df = pd.read_csv("HousePricePrediction.csv")
+clean_df = df[df['SalePrice'] <150000] #clean the data hows saleprice is less then 150k
+#print(clean_df.info())
+
+sub_set = clean_df[['Id', 'LotArea', 'TotalBsmtSF', 'SalePrice']] # Create a subset with the following columns
+#print(sub_set)
+
+sub_set.to_excel("subset house price.xlsx") #Expoert the subset in excel format
+
+print("File exported")
+sub_set.info()
